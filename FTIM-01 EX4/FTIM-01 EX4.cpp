8f5042b1626f4ec3b9decbdcacd88548ae0408b0
Xico <60393779+4qu3l3Tr0p4@users.noreply.github.com>
2020-02-04 14:52:32 +0000

    Add files via upload

---
+++ FTIM-01 EX4/FTIM-01 EX4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1;
+	int num2;
+
+	cout << "Insira dois numeros inteiros para serem somados: " << endl;
+	cin >> num1;
+	cin >> num2;
+	cout << "A sua soma= " << num1 + num2 << endl;
+	
+	
+}
+
+
