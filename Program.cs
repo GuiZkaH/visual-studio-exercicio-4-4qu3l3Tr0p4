3ccbfffaa360eb484d45e2781c312d840602aca8
GuiZkaH <52414162+GuiZkaH@users.noreply.github.com>
2020-02-06 16:06:49 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuiZkaH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+");
+            Console.WriteLine("O que queres fazer agora?");
+        }
+    }
+}
